1a5123c2a463139d5f3c6ade5662041fb4a26b62
Tab <tab@controldeMacBook-Pro.local>
2024-05-21 15:38:03 +0800

    startfrom: kubectl_apply

---
+++ scripts/step_runner.sh
@@ -0,0 +1,14 @@
+# scripts/step_runner.sh
+#!/bin/bash
+
+STEP_NAME=$1
+echo "Evaluating step: $STEP_NAME"
+
+if [ "$START_FROM" != "none" ] && [ "$START_FROM" != "$STEP_NAME" ]; then
+  echo "Skipping step: $STEP_NAME"
+  exit 0
+fi
+
+echo "Executing step: $STEP_NAME"
+shift
+exec "$@"
