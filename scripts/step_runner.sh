0137db92d9b3cd226674ad75a60c01a12e2c8f51
Tab <tab@controldeMacBook-Pro.local>
2024-05-22 10:25:58 +0800

    startfrom: gke_deploy

--- scripts/step_runner.sh
+++ scripts/step_runner.sh
@@ -2,9 +2,15 @@
 
 STEP_NAME=$1
 echo "Evaluating step: $STEP_NAME"
-echo "START FROM step: $START_FROM"
 
-if [ "$START_FROM" != "none" ] && [ "$START_FROM" != "$STEP_NAME" ]; then
+# Load the START_FROM variable
+if [ -f /workspace/env_vars.txt ]; then
+  . /workspace/env_vars.txt
+else
+  START_FROM="none"
+fi
+
+if [ "$START_FROM" != "none" ] && [ "$START_FROM" != "$STEP_NAME" ] && [ "$STEP_NAME" != "check_commit_message" ]; then
   echo "Skipping step: $STEP_NAME"
   exit 0
 fi
